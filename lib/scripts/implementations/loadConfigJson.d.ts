c8fce47f8a140b056dd13c7bc0d46eb92846336b
Geir Sagberg <geir.sagberg@gmail.com>
2022-06-30 17:28:10 +0200

    Add loadConfigJson separate method

--- lib/scripts/implementations/loadConfigJson.d.ts
+++ lib/scripts/implementations/loadConfigJson.d.ts
@@ -1,1 +1,6 @@
+declare type ProjectsConfig = Omit<Record<string, string>, 'project_numbers'> & {
+    project_numbers: Record<string, string>;
+};
 export declare function loadConfigJson(projectName: string): Promise<string>;
+export declare function loadProjectsConfig(projectName: string): Promise<ProjectsConfig>;
+export {};
--- src/scripts/implementations/loadConfigJson.ts
+++ src/scripts/implementations/loadConfigJson.ts
@@ -1,6 +1,16 @@
 import { ProjectsClient } from '@google-cloud/resource-manager'
 
+type ProjectsConfig = Omit<Record<string, string>, 'project_numbers'> & {
+  project_numbers: Record<string, string>
+}
+
 export async function loadConfigJson(projectName: string) {
+  const projectConfig = await loadProjectsConfig(projectName)
+
+  return JSON.stringify(projectConfig, null, 2)
+}
+
+export async function loadProjectsConfig(projectName: string) {
   const client = new ProjectsClient()
 
   const projects = await client
@@ -16,15 +26,13 @@ export async function loadConfigJson(projectName: string) {
         return config
       }
       const env = environment.toUpperCase()
-      config[env] = project.projectId
-      config.project_numbers[env] = project.name?.split('/')[1]
+      config[env] = project.projectId!!
+      config.project_numbers[env] = project.name?.split('/')[1]!!
       return config
     },
     {
       project_numbers: {},
-    } as any
+    } as ProjectsConfig
   )
-
-  const configJson = JSON.stringify(projectConfig, null, 2)
-  return configJson
+  return projectConfig
 }
