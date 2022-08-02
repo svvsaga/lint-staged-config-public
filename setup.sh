bbfd2d3809cb38f2d4dd55a3441ceb3cb7f7bcfe
Geir Sagberg <geir.sagberg@gmail.com>
2022-08-02 11:12:18 +0200

    Update setup.sh
    Setup lint-staged config as part of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 npm set-script prepare "npx husky install"
 npm run prepare
 npx husky set .husky/pre-commit "npx lint-staged"
+
+echo "import { lintStagedConfig } from 'node-modules-public'
+
+export default lintStagedConfig()" > lint-staged.config.mjs
